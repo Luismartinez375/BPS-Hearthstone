6ef9eeea28df37d068ea844f5d5931021b6dd322
bear <manbearpig375@gmail.com>
2023-07-27 23:10:41 -0500

    feat: configuring googlemaps api

--- src/app/components/googlemaps/maps.tsx
+++ src/app/components/googlemaps/maps.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { useEffect, useState } from 'react';
 
 const containerStyle = {
   width: '80%',
@@ -12,22 +13,73 @@ const center = {
 };
 
 export default function GoogleMaps() {
-  const key = process.env.MAPS_KEY;
+  const [userPosition, setUserPosition] = useState<{ lat: number; lng: number } | null>(null);
+  const [nearbyCardShops, setNearbyCardShops] = useState<{ lat: number; lng: number }[]>([]);
+  const center = userPosition || { lat: 0, lng: 0 }; // Center the map on the user's location if available
+
+  useEffect(() => {
+    // Fetch the user's current location using the browser's geolocation API
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserPosition({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+        }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    // Fetch nearby card shops using Google Maps Places API when userPosition changes
+    if (userPosition) {
+      const key = process.env.MAPS_KEY;
+      const placesAPIKey = 'AIzaSyAlPThq93vQn6dasXoUctWM7OkhWxyhs3c';
+      const radius = 1000; // Define the search radius (in meters) around the user's location
+
+      const request = {
+        location: new window.google.maps.LatLng(userPosition.lat, userPosition.lng),
+        radius: radius,
+        type: 'store', // Adjust the type according to the card shops category in the Places API
+        keyword: 'cards', // You can use other keywords specific to card shops
+        key: placesAPIKey,
+      };
+
+      const service = new window.google.maps.places.PlacesService(document.createElement('div'));
+
+      service.nearbySearch(request, (results, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          const cardShops = results?.map((result) => ({
+            lat: result.geometry?.location?.lat() || 0,
+            lng: result.geometry?.location?.lng() || 0,
+          })) || [];
+      
+          setNearbyCardShops(cardShops);
+        }
+      });
+  }}
+  , [userPosition]);
+  const key = process.env.MAPS_KEY
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: key ? key : '',
+    googleMapsApiKey: 'AIzaSyAlPThq93vQn6dasXoUctWM7OkhWxyhs3c',
   });
-
-  const handleZoomChanged = () => {
-    console.log('Zoom');
-  };
 
   return isLoaded ? (
     <>
       <div className="overflow-y-none">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          <Marker position={center} />
+        <LoadScript googleMapsApiKey={'AIzaSyAlPThq93vQn6dasXoUctWM7OkhWxyhs3c'} libraries={['places']}>
+        <GoogleMap mapContainerStyle={containerStyle} center={userPosition ? userPosition : undefined} zoom={10}>
+        {userPosition && <Marker position={{ lat: userPosition.lat, lng: userPosition.lng }} />}
+        {nearbyCardShops.map((shop, index) => (
+          <Marker key={index} position={{ lat: shop.lat, lng: shop.lng }} />
+        ))}
         </GoogleMap>
+        </LoadScript>
       </div>
     </>
   ) : (

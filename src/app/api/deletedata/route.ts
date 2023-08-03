5f2503c5a6ebefceb5b12cdad5feb06d5d567086
luismartinez375 <luisleonardomartinez375@live.com>
2023-08-03 17:00:36 -0500

    feat: postgres

--- src/app/api/deletedata/route.ts
+++ src/app/api/deletedata/route.ts
@@ -1,7 +1,7 @@
 import { deleteRecord } from '../../../../lib/db';
 
 // Function to delete a Hearthstone card by id
-export const deleteCardById = async (id: string): Promise<void> => {
+export const DELETE = async (id: string): Promise<void> => {
   const queryText = 'DELETE FROM favorites WHERE id = $1';
   const values = [id];
   await deleteRecord(queryText, values);

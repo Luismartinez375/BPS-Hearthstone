7e94869ffc999b7d09212257da313ece9a2d7d26
luismartinez375 <luisleonardomartinez375@live.com>
2023-08-03 17:04:52 -0500

    feat: postgres

--- src/app/api/deletedata/route.ts
+++ src/app/api/deletedata/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server';
 import { deleteRecord } from '../../../../lib/db';
 
 // Function to delete a Hearthstone card by id
-export async function DELETE(id: string) {
+export async function DELETE() {
   const queryText = 'DELETE FROM favorites WHERE id = $1';
-  const values = [id];
+  const values = ['10'];
   await deleteRecord(queryText, values);
   NextResponse.json('deleted');
 }
